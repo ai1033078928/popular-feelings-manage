a973d893654919952959d4a0a26d6b18cd8c33b5
晚安 <a103308928@gmail.com>
2020-06-01 18:10:25 +0800

    测试https提交

---
+++ popular-feelings-manage-DataSubSystem/DataLoader/src/main/java/com/hrbu/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.hrbu;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello啊, 树哥");
+    }
+}
